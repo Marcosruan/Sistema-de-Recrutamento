a4b783aaa489e24c881a8a2e5b4071ce94a8a6db
MiguelLuan <br.miguelcurseiro1000@gmail.com>
2026-07-28 13:14:07 -0300

    feat: implementar atributos e metodos da classe abstrata Usuario

--- src/main/modelos/usuario/Usuario.java
+++ src/main/modelos/usuario/Usuario.java
@@ -1,5 +1,49 @@
 package main.modelos.usuario;
 
-public class Usuario {
+import java.util.Objects;
 
+public abstract class Usuario {
+	private String nome;
+	private String cpf;
+	private String email;
+	private String senha;
+	
+	public Usuario(String nome,String cpf,String email,String senha) {
+		this.nome = nome;
+		this.cpf = cpf;
+		this.email = email;
+		this.senha = senha;
+	}
+	public boolean altenticar(String email,String senha) {
+		if(this.email.equals(email) && this.senha.equals(senha)) {
+			return true;
+		}
+		return false;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	@Override
+	public int hashCode() {
+		return Objects.hash(cpf);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Usuario other = (Usuario) obj;
+		return Objects.equals(cpf, other.cpf);
+	}
+	
+	
 }
