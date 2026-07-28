82b1b38072d3a1fbf9bc90d669bd0fdf6d5d4243
Miguel Luan <br.miguelcurseiro1000@gmail.com>
2026-07-28 13:26:03 -0300

    Revert "Feat/miguel/issue#3"

--- src/main/modelos/interfaces/ControleAcesso.java
+++ src/main/modelos/interfaces/ControleAcesso.java
@@ -1,12 +1,5 @@
 package main.modelos.interfaces;
 
-import main.modelos.usuario.Usuario;
-
 public interface ControleAcesso {
 
-	boolean podeVisualizarUsuario(Usuario usuario);
-	
-	boolean podeEditar(Usuario usuario);
-	
-	boolean podeRemover(Usuario usuario);
 }
