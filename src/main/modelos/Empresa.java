dcdec2f350af4984abdd226c8add6d44d6c5c497
jjonnas-67 <zejonnas.as@gmail.com>
2026-07-29 22:03:03 -0300

    Criar estrutura inicial da classe Empresa

--- src/main/modelos/Empresa.java
+++ src/main/modelos/Empresa.java
@@ -1,5 +1,38 @@
 package main.modelos;
 
-public class Empresa {
 
+import main.modelos.interfaces.ControleAcesso;
+import main.modelos.usuario.Usuario;
+
+public class Empresa implements ControleAcesso {
+    String nome;
+    String descricao;
+    String cidade;
+    String setor;
+
+    public Empresa(String nome, String descricao, String cidade, String setor) {
+        this.nome = nome;
+        this.descricao = descricao;
+        this.cidade = cidade;
+        this.setor = setor;
+    }
+
+    public void exibirInfoPublica(){
+
+    }
+
+    @Override
+    public boolean podeVisualizarUsuario(Usuario usuario) {
+        return false;
+    }
+
+    @Override
+    public boolean podeEditar(Usuario usuario) {
+        return false;
+    }
+
+    @Override
+    public boolean podeRemover(Usuario usuario) {
+        return false;
+    }
 }
