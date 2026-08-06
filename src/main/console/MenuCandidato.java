aa7ef83583344d91996639886059a0226fe74ffd
Marcos Ruan <113126971+Marcosruan@users.noreply.github.com>
2026-08-06 14:44:42 -0300

    Add submenu display methods to candidate and recruiter menus

--- src/main/console/MenuCandidato.java
+++ src/main/console/MenuCandidato.java
@@ -13,8 +13,44 @@ public class MenuCandidato implements EstrategiaMenuUsuario {
 	
 	@Override
 	public void menu() {
-		// TODO Auto-generated method stub
-		
+		exibirMenu();
+	}
+	
+	private void exibirMenu() {
+		System.out.println("=== Menu Candidato ===");
+		System.out.println("1 - Curriculo");	
+		System.out.println("2 - Candidaturas");	
+		System.out.println("3 - Dados pessoais");
+		System.out.println("0 - Sair");
+	}
+	
+	private void exibirMenuCandidatura() {
+		System.out.println("=== Menu Candidatura ===");
+		System.out.println("1 - Ver vagas");
+		System.out.println("2 - Ver candidaturas");
+		System.out.println("3 - Se candidatar à vaga");
+		System.out.println("4 - Remover candidatura");
+		System.out.println("0 - Sair");
+	}
+	
+	private void exibirMenuCurriculo() {
+		System.out.println("=== Menu Curriculo ===");
+		System.out.println("1 - Cadastrar currículo");
+		System.out.println("2 - Ver curriculo");
+		System.out.println("3 - Alterar formação");
+		System.out.println("4 - Alterar experiência");
+		System.out.println("5 - Alterar habilidades");
+		System.out.println("6 - Alterar idiomas");
+		System.out.println("7 - Alterar cidade da vaga");
+		System.out.println("0 - Sair");
+	}
+	
+	private void exibirMenuDadosPessoais() {
+		System.out.println("=== Dados Pessoais ===");
+		System.out.println("1 - Ver dados pessoais");
+		System.out.println("2 - Alterar nome");
+		System.out.println("3 - Alterar senha");
+		System.out.println("0 - Sair");
 	}
 
 	@Override
--- src/main/console/MenuRecrutador.java
+++ src/main/console/MenuRecrutador.java
@@ -13,8 +13,48 @@ public class MenuRecrutador implements EstrategiaMenuUsuario {
 
 	@Override
 	public void menu() {
-		// TODO Auto-generated method stub
-		
+		exibirMenu();
+	}
+
+	private void exibirMenu() {
+		System.out.println("=== Menu Recrutador ===");
+		System.out.println("1 - Vagas");	
+		System.out.println("2 - Candidaturas");	
+		System.out.println("3 - Dados pessoais");	
+		System.out.println("0 - Sair");
+	}
+
+	private void exibirMenuVaga() {
+		System.out.println("=== Menu Vaga ===");
+		System.out.println("1 - Cadastrar vaga");
+		System.out.println("2 - Ver vagas");
+		System.out.println("3 - Abrir vaga");
+		System.out.println("4 - Fechar vaga");
+		System.out.println("5 - Alterar título da vaga");
+		System.out.println("6 - Alterar descrição da vaga");
+		System.out.println("7 - Alterar requisitos da vaga");
+		System.out.println("8 - Alterar salário da vaga");
+		System.out.println("9 - Alterar cidade da vaga");
+		System.out.println("0 - Sair");
+	}
+	
+	private void exibirMenuCandidatura() {
+		System.out.println("=== Menu Candidatura ===");
+		System.out.println("1 - Ver candidaturas");
+		System.out.println("2 - Colocar candidatura em análise");
+		System.out.println("3 - Marcar entrevista de candidato");
+		System.out.println("4 - Aprovar candidatura");
+		System.out.println("5 - Reprovar candidatura");
+		System.out.println("0 - Sair");
+	}
+	
+	private void exibirMenuDadosPessoais() {
+		System.out.println("=== Dados Pessoais ===");
+		System.out.println("1 - Ver dados pessoais");
+		System.out.println("2 - Alterar nome");
+		System.out.println("3 - Alterar senha");
+		System.out.println("4 - Alterar empresa");
+		System.out.println("0 - Sair");
 	}
 
 	@Override
