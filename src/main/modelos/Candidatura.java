4b7eaf82c3d2ee84c42183b0b7e51a58f5128cde
Carlos Henrique <www.carlos4k@gmail.com>
2026-08-08 13:12:34 -0300

    feat: implementar comportamentos de candidatura

--- src/main/modelos/Candidatura.java
+++ src/main/modelos/Candidatura.java
@@ -10,11 +10,36 @@ public class Candidatura {
 	private Candidato candidato;
 	private Vaga vaga;
 	
-	public Candidatura() {
-		// TODO Auto-generated constructor stub
+	public Candidatura(StatusCandidatura status, Candidato candidato, Vaga vaga) {
+		this.status = StatusCandidatura.EM_ANALISE;
+		this.candidato = candidato;
+		this.vaga = vaga;
+	}
+
+	public Candidato getCandidato() {
+		return candidato;
+	}
+
+	public void setCandidato(Candidato candidato) {
+		this.candidato = candidato;
+	}
+
+	public Vaga getVaga() {
+		return vaga;
+	}
+
+	public void setVaga(Vaga vaga) {
+		this.vaga = vaga;
+	}
+
+	public StatusCandidatura alterarStatus(StatusCandidatura novoStatus) {
+		this.status = novoStatus;
+		return this.status;
+	}
+
+	@Override
+	public String toString() {
+		return "[" + vaga.getTitulo() + "] Candidatura de: " + candidato.toString() + "\nStatus da cadidatura: " + status;
 	}
 	
-	public boolean alterarStatus(StatusCandidatura novoStatus) {
-		return false;
-	}
 }
