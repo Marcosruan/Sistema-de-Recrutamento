fe611ec32d8851ba85e57ae9e996856b0f0912c4
Miguel Luan <br.miguelcurseiro1000@gmail.com>
2026-08-10 13:03:27 -0300

    feat: Refactor Candidato class to improve curriculum management

--- src/main/modelos/usuario/Candidato.java
+++ src/main/modelos/usuario/Candidato.java
@@ -17,21 +17,71 @@ public class Candidato extends Usuario {
 		this.candidaturas = new ArrayList<Candidatura>();
 	}
 	
-	public void cadastrarCurriculo(Curriculo curriculo) {
+	private void validarCurriculo() throws IllegalStateException{
+		if(curriculo == null) {
+			throw new IllegalStateException("O candidato ainda não possui um currículo cadastrado.");
+		}
+	}
+	
+	public void cadastrarCurriculo(Curriculo curriculo) throws IllegalArgumentException{
+		if(curriculo == null) {
+	        throw new IllegalArgumentException("O currículo não pode ser nulo.");
+		}
 		this.curriculo = curriculo;
 	}
-	public void editarCurriculo(Curriculo novoCurriculo) {
-	    if (novoCurriculo == null) {
-	        throw new IllegalArgumentException("O novo currículo não pode ser nulo.");
-	    }
-	    this.curriculo = novoCurriculo;
+	
+	public void adicionarExperienciaCurriculo(String novaExperiencia) {
+        validarCurriculo();
+        this.curriculo.adicionarExperiencia(novaExperiencia); 
 	}
-	public void candidatarVaga(Candidatura candidatura) {
+	
+	public void adicionarFormacaoCurriculo(String novaFormacao) {
+        validarCurriculo();
+        this.curriculo.adicionarFormacao(novaFormacao); 
+	}
+	
+	public void adicionarHabilidadeCurriculo(String novaHabilidade) {
+        validarCurriculo();
+        this.curriculo.adicionarHabilidade(novaHabilidade);
+	}
+	
+	public void adicionarIdiomasCurriculo(String novoIdioma) {
+        validarCurriculo();
+        this.curriculo.adicionarIdiomas(novoIdioma); 
+	}
+	
+	public void editarFormacaoCurriculo(String formacaoAntiga, String formacaoNova) throws IllegalArgumentException, IllegalStateException{
+        validarCurriculo();
+        this.curriculo.editarFormacao(formacaoAntiga, formacaoNova); 
+    }
+	
+	public void editarHabilidadeCurriculo(String habilidadeAntiga, String habilidadeNova) throws IllegalArgumentException, IllegalStateException{
+        validarCurriculo();
+        this.curriculo.editarHabilidade(habilidadeAntiga, habilidadeNova); 
+	}
+	
+	public void editarIdiomasCurriculo(String idiomaAntigo, String idiomaNovo) throws IllegalArgumentException, IllegalStateException {
+        validarCurriculo();
+        this.curriculo.editarIdiomas(idiomaAntigo, idiomaNovo); 
+	}
+	
+	public void deletarCurriculo()  throws IllegalStateException{
+		validarCurriculo();
+		curriculo = null;
+	}
+	
+	public Curriculo getCurriculo() {
+		return curriculo;
+	}
+	
+	public void candidatarVaga(Candidatura candidatura) throws IllegalArgumentException, IllegalStateException {
 		candidaturas.add(candidatura);
 	}
-	public void cancelarCandidatura() {
-		
+	
+	public List<Candidatura> getCandidaturas() {
+		return candidaturas;
 	}
+
 	public String visualizarCandidaturas() {
 		if(candidaturas.size() == 0) return "Sem candidaturas registrada!";
 		List<String> TextoCandidaturas = new ArrayList<String>();
@@ -54,7 +104,13 @@ public class Candidato extends Usuario {
 	public boolean ehPermitidoAlterarVagas() {
 		return false;
 	}
-	
+	@Override
+	public boolean ehPermitidoManipularCurriculo() {
+		return true;
+	}
 
-
+	@Override
+	public TipoUsuario getTipo() {
+		return TipoUsuario.CANDIDATO;
+	}
 }
