ff5bba13f8d751d3ad13ada902eb5a907552ed71
Miguel Luan <br.miguelcurseiro1000@gmail.com>
2026-08-10 13:08:27 -0300

    feat: Enhance StatusCandidatura with string values
    Added string representation and getter method for status.

--- src/main/modelos/enums/StatusCandidatura.java
+++ src/main/modelos/enums/StatusCandidatura.java
@@ -1,8 +1,19 @@
 package main.modelos.enums;
 
 public enum StatusCandidatura {
-	EM_ANALISE,
-	ENTREVISTA,
-	APROVADO,
-	REPROVADO;
+	EM_ANALISE("Em análise"),
+	ENTREVISTA("Em entrevista"),
+	APROVADO("Aprovada"),
+	REPROVADO("Reprovada"),
+	CANCELADA("Cancelada");
+	
+	private String texto;
+	
+	private StatusCandidatura(String texto) {
+		this.texto = texto;
+	}
+
+	public String getTexto() {
+		return texto;
+	}
 }
